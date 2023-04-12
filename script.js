7da57337d00b3270d84643ab0a34499f03e39eb5
Aaron Chong <aarncks23@hotmail.com>
2023-04-12 19:40:40 +0800

    Add button to change grid size

--- script.js
+++ script.js
@@ -1,22 +1,30 @@
-const container = document.querySelector('.container');
-const gridSize = 16;
-let rows = document.createDocumentFragment();
+const btn = document.querySelector('#btn');
+let gridSize = 2;
+btn.addEventListener('click', () => {
+    gridSize = Number(prompt("Enter grid size:"));
+    const container = document.querySelector('.container');
+    container.replaceChildren();
+    createGrid(gridSize, container);
+});
 
-for (let i = 0; i < gridSize; i++) {
-    let row = document.createElement('div');
-    row.classList.add('square');
-    row.style.cssText = "flex-basis: " + (container.offsetWidth / gridSize);
-    for (let j = 0; j < gridSize; j++) {
-        let square = document.createElement('div');
-        square.classList.add('square');
-        square.style.cssText = "flex-basis: " + (container.offsetWidth / gridSize);
-        square.addEventListener("mouseenter", (e) => {
-            e.target.style.cssText = "background-color: black;";
-        });
-        row.appendChild(square);
+function createGrid(gridSize, container) {
+    let rows = document.createDocumentFragment();
+
+    for (let i = 0; i < gridSize; i++) {
+        let row = document.createElement('div');
+        row.classList.add('square');
+        row.style.cssText = "flex-basis: " + (container.offsetWidth / gridSize);
+        for (let j = 0; j < gridSize; j++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            square.style.cssText = "flex-basis: " + (container.offsetWidth / gridSize);
+            square.addEventListener("mouseenter", (e) => {
+                e.target.style.cssText = "background-color: black;";
+            });
+            row.appendChild(square);
+        }
+        rows.appendChild(row);
     }
-    rows.appendChild(row);
+
+    container.appendChild(rows);
 }
-
-container.appendChild(rows);
-
