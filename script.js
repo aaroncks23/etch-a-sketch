3c50982f0778e4ef8e9ec83e3c175e0fb84b4e7d
Aaron Chong <aarncks23@hotmail.com>
2023-04-12 16:43:19 +0800

    Add grid

---
+++ script.js
@@ -0,0 +1,21 @@
+const container = document.querySelector('.container');
+const gridSize = 16;
+let rows = document.createDocumentFragment();
+
+for (let i = 0; i < gridSize; i++) {
+    let row = document.createElement('div');
+    row.classList.add('square');
+    row.style.cssText = "flex-basis: " + (container.offsetWidth / gridSize);
+    for (let j = 0; j < gridSize; j++) {
+        let square = document.createElement('div');
+        square.classList.add('square');
+        square.style.cssText = "flex-basis: " + (container.offsetWidth / gridSize);
+        row.appendChild(square);
+    }
+    rows.appendChild(row);
+}
+
+
+container.appendChild(rows);
+
+
